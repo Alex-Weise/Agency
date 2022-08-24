b484293fc466be73f2c4c2eadd3ef559b8d7912a
Alex-Weise <Sannek65@gmail.com>
2022-08-24 18:59:51 +0300

    Добавил компонент с контентом what we do

--- src/components/WthaWeDo/index.tsx
+++ src/components/WthaWeDo/index.tsx
@@ -1,14 +1,66 @@
 import styles from "./styles.module.scss";
+import website from "../../assets/link/website.png";
+import computer from "../../assets/link/computer.png";
+import mobile from "../../assets/link/mobile-phone.png";
+import rocket from "../../assets/link/rocket.png";
+import heart from "../../assets/link/icon-heart.png";
 
 const WhatWeDo = () => {
     return (
         <section className={styles.container}>
             <div className={styles.wrapper}>
                 <div className={styles.blue} />
+                <div className={styles.line} />
             </div>
+            <div className={styles.dotted} />
             <p className={styles.title1}>What We Do</p>
             <p className={styles.title2}>For Your Business</p>
-            <div className={styles.line} />
+            <div className={styles.cards}>
+                <div className={styles.card1}>
+                    <div className={styles.card_icon} style={{backgroundColor: "#FFDED4"}}>
+                        <img src={website} alt="website" />
+                    </div>
+                    <h4 className={styles.card_title}>WECSITE DESIGN</h4>
+                    <p className={styles.card_text}>Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s.</p>
+                </div>
+                <div className={styles.card2}>
+                    <div className={styles.card_icon} style={{backgroundColor: "#FFEED4"}}>
+                        <img src={computer} alt="compute" />
+                    </div>
+                    <h4 className={styles.card_title}>WECSITE DESIGN</h4>
+                    <p className={styles.card_text}>Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s.</p>
+                </div>
+                <div className={styles.card3}>
+                    <div className={styles.card_icon} style={{backgroundColor: "#D4F0FF"}}>
+                        <img src={mobile} alt="mobile phone" />
+                    </div>
+                    <h4 className={styles.card_title}>WECSITE DESIGN</h4>
+                    <p className={styles.card_text}>Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s.</p>
+                </div>
+                <div className={styles.card4}>
+                    <div className={styles.card_icon} style={{backgroundColor: "#FFD4D4"}}>
+                        <img src={rocket} alt="rocket" />
+                    </div>
+                    <h4 className={styles.card_title}>WECSITE DESIGN</h4>
+                    <p className={styles.card_text}>Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s.</p>
+                </div>
+                <div className={styles.card5}>
+                    <div className={styles.card_icon} style={{backgroundColor: "#FDC021"}}>
+                        <img src={heart} alt="heart" />
+                    </div>
+                    <h4 className={styles.card_title}>WECSITE DESIGN</h4>
+                    <p className={styles.card_text}>Lorem Ipsum has been the industry's 
+                    standard dummy text ever since the 1500s.</p>
+                </div>
+            </div>
+            <div className={styles.buttons}>
+                <button className={styles.back} type="button">{"<"}</button>
+                <button className={styles.next} type="button">{">"}</button>
+            </div>
         </section>
     )
 };
