f499bf452ae0a3503fa910cec2d20719bb0ff283
Alex-Weise <Sannek65@gmail.com>
2022-08-26 01:58:28 +0300

    Добавил скрол по кнопкам навигации

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,44 @@ import styles from "./styles.module.scss";
 import facebook from "../../assets/link/facebook.png";
 import insta from "../../assets/link/insta.png";
 import linkIn from "../../assets/link/linkIn.png";
+import { useState } from "react";
 
 const Header = () => {
+    const [activeLine, setActive] = useState("Home");
+
+    const scrollTo = (str:string) => {
+        let width = window.screen.width;
+        if ( str === "About Us") {
+            if (width > 1024) {
+                window.scrollTo(0, 1500);
+            } else if (width <= 1024 && width > 767) {
+                window.scrollTo(0, 1160);
+            } else  {
+                window.scrollTo(0, 850);
+            }
+        }
+    };
+    // scrollTo("About Us");
 
     return (
         <header className={styles.header}>
             <div className={styles.nav}>
-                <p>Home</p>
-                <p>About Us</p>
-                <p>Services</p>
-                <p>Contact Us</p>
+                <div style={{position: "relative"}}
+                    onClick={() => setActive("Home")}>
+                        Home{activeLine === "Home" && <ActiveLine />}
+                </div>
+                <div style={{position: "relative"}}
+                    onClick={() => {setActive("About Us"); scrollTo("About Us")}}>
+                        About Us{activeLine === "About Us" && <ActiveLine />}
+                </div>
+                <div style={{position: "relative"}}
+                    onClick={() => setActive("Services")}>
+                        Services{activeLine === "Services" && <ActiveLine />}
+                </div>
+                <div style={{position: "relative"}}
+                    onClick={() => setActive("Contact Us")}>
+                        Contact Us{activeLine === "Contact Us" && <ActiveLine />}
+                </div>
             </div>
             <div className={styles.logo}>
                 <h1>Dream Agency</h1>
@@ -32,3 +60,9 @@ const Header = () => {
 };
 
 export { Header };
+
+function ActiveLine () {
+    return (
+        <div className={styles.activeLine} />
+    )
+}
