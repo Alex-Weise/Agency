84125ac4009482f37aeca2cbbf221a5c2fe9ce21
Alex-Weise <Sannek65@gmail.com>
2022-08-25 23:42:10 +0300

    Настроил слайдер и адаптацию

--- src/components/OurWork/index.tsx
+++ src/components/OurWork/index.tsx
@@ -20,11 +20,13 @@ const OurWork = () => {
         const cards = document.getElementById("cards2");
         if (cards && count <= 100 ) {
             setCount(prev => prev + 20);
-            if (width > 576) {
+            if (width > 1180) {
                 cards.style.cssText = `transform: translate(${22 - count}%, 0)`;
-            } else if (width <= 398) {
-                cards.style.cssText = `transform: translate(${22 - (count * 2.2)}%, 0)`;
-            } else {cards.style.cssText = `transform: translate(${22 - (count * 1.5)}%, 0)`;}
+            } else if (width <= 576 && width > 400) {
+                cards.style.cssText = `transform: translate(${22 - (count * 1.2)}%, 0)`;
+            } else if (width <= 400) {
+                cards.style.cssText = `transform: translate(${22 - (count * 1.7)}%, 0)`;
+            } else {cards.style.cssText = `transform: translate(${22 - (count * 1.4)}%, 0)`;}
         } 
     }
 
@@ -33,9 +35,12 @@ const OurWork = () => {
         const cards = document.getElementById("cards2");
         if (cards && count > 20) {
             setCount(prev => prev - 20);
-            if (width > 576) {cards.style.cssText = `transform: translate(${(140 - count) - 78}%, 0)`;
-            } else if (width <= 398) {cards.style.cssText = `transform: translate(${((105 - count) * 2.2) - 145}%, 0)`;
-            } else {cards.style.cssText = `transform: translate(${((105 - count) * 1.5) - 92.5}%, 0)`;}
+            if (width > 1180) {cards.style.cssText = `transform: translate(${(140 - count) - 78}%, 0)`;
+            } else if (width <= 576 && width > 400) {
+                cards.style.cssText = `transform: translate(${((140 - count) * 1.2) - 98}%, 0)`;
+            } else if (width <= 400) {
+                cards.style.cssText = `transform: translate(${((140 - count) * 1.7) - 148}%, 0)`;
+            } else {cards.style.cssText = `transform: translate(${((140 - count) * 1.4) - 118}%, 0)`;}
         }
     }
 
@@ -56,8 +61,8 @@ const OurWork = () => {
             <div className={styles.cards} id="cards2">
                 <div className={styles.card}>
                     <div className={styles.image}>
-                        <img src={web1} alt="Page 1" />
-                        <img src={mob1} alt="Mobile 1" />
+                        <img src={web1} className={styles.img1} alt="Page 1" />
+                        <img src={mob1} className={styles.img2} alt="Mobile 1" />
                     </div>
                     <div className={styles.discribe}>
                         <h5>UI Soup</h5>
@@ -66,8 +71,8 @@ const OurWork = () => {
                 </div>
                 <div className={styles.card}>
                    <div className={styles.image}>
-                        <img src={web2} alt="Page 2" />
-                        <img src={mob2} alt="Mobile 2" />
+                        <img src={web2} className={styles.img1} alt="Page 2" />
+                        <img src={mob2} className={styles.img2} alt="Mobile 2" />
                     </div>
                     <div className={styles.discribe}>
                         <h5>Goldcrown Labs</h5>
@@ -76,8 +81,8 @@ const OurWork = () => {
                 </div>
                 <div className={styles.card}>
                     <div className={styles.image}>
-                        <img src={web3} alt="Page 3" />
-                        <img src={mob2} alt="Mobile 3" />
+                        <img src={web3} className={styles.img1} alt="Page 3" />
+                        <img src={mob2} className={styles.img2} alt="Mobile 3" />
                     </div>
                     <div className={styles.discribe}>
                         <h5>Close Concierge</h5>
@@ -86,8 +91,8 @@ const OurWork = () => {
                 </div>
                 <div className={styles.card}>
                     <div className={styles.image}>
-                        <img src={web1} alt="Page 4" />
-                        <img src={mob1} alt="Mobile 4" />
+                        <img src={web1} className={styles.img1} alt="Page 4" />
+                        <img src={mob1} className={styles.img2} alt="Mobile 4" />
                     </div>
                     <div className={styles.discribe}>
                         <h5>UI Soup</h5>
@@ -96,8 +101,8 @@ const OurWork = () => {
                 </div>
                 <div className={styles.card}>
                     <div className={styles.image}>
-                        <img src={web2} alt="Page 5" />
-                        <img src={mob2} alt="Mobile 5" />
+                        <img src={web2} className={styles.img1} alt="Page 5" />
+                        <img src={mob2} className={styles.img2} alt="Mobile 5" />
                     </div>
                     <div className={styles.discribe}>
                         <h5>Goldcrown Labs</h5>
